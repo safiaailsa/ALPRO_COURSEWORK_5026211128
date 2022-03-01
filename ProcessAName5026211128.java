192b49d8f3ed0b8f858c0245757f77b413cde034
safiaailsa <100075671+safiaailsa@users.noreply.github.com>
2022-03-01 23:49:45 +0700

    Update ProcessAName5026211128.java

--- ProcessAName5026211128.java
+++ ProcessAName5026211128.java
@@ -1,3 +1,4 @@
+import java.lang.String;
 import java.util.Scanner;
 
 public class ProcessAName5026211128{
@@ -6,13 +7,14 @@ public class ProcessAName5026211128{
 
 	Scanner sc = new Scanner(System.in);
 		System.out.print("Type your name: ");
+	  
 	String Name = sc.nextLine();
 
 	int lastIndex = Name.lastIndexOf(" ");
 		System.out.println(lastIndex);
 
-	int firstname = Name.lastIndexOf(" ");
-	String lastname = Name.substring(firstname+1);
+	int a = Name.lastIndexOf(" ");
+	String lastname = Name.substring(a+1);
 		System.out.println("Your name is " +lastname+ ", " +Name.charAt(0)+ ". ");
   }
 }
